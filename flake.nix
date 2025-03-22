d62e3e2373d6fef46f9f60a8af33b5c42e7a19d0
Dane Rieber <danerieber@gmail.com>
2025-03-21 19:08:01 -0500

    add template as output

--- flake.nix
+++ flake.nix
@@ -3,5 +3,9 @@
     { ... }:
     {
       init = import ./init.nix;
+      templates.default = {
+        path = ./template;
+        description = "minimal template with onix directory structure";
+      };
     };
 }
